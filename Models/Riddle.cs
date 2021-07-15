55a0a7911732621e3f223b3690b555be7710fcf1
Brandon <brandonaltermatt@gmail.com>
2021-07-15 10:41:39 -0700

    Updated view basic information, added riddle class

---
+++ Models/Riddle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace riddle_crud_application.Models
+{
+    public class Riddle
+    {
+        public int id { get; set; }
+        public string RiddleQuestion { get; set; }
+        public string RiddleAnswer { get; set; }
+    }
+}
